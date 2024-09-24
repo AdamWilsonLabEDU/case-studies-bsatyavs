4a8a430df604414fad1a3d8c868e5dcbfeec6809
bsatyavs <bsatyavs@gmail.com>
2024-09-24 14:43:05 -0400

    used iris dataset

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,23 @@
 data(iris)
+#print dataset
+print(iris)
+
+#calculate the mean of Petal.length
+petal_length_mean <- mean(iris$Petal.Length)
+print(petal_length_mean)
+
+#plot the histogram of petal.length
+hist(iris$Petal.Length)
+
+#install ggplot2 library
+#Plot the Geom histogram of petal.length
+ggplot(data = iris, aes(x = Petal.Length)) +
+  geom_histogram(color = "white", fill = "red")
+
+
+==summary(iris)
+
+ggplot(data = iris, aes(x = Petal.Length)) +
+  geom_histogram(bins=50)
+
 
