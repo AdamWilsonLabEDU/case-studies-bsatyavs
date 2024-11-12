1cae1a9f0babd0f7f487564c642684284f4851bc
bsatyavs <bsatyavs@gmail.com>
2024-11-11 21:55:15 -0500

    Add files via upload

---
+++ week_04/case_study 4.R
@@ -0,0 +1,30 @@
+install.packages("tidyverse")
+install.packages("nycflights13")
+library(tidyverse)
+library(nycflights13)
+view(airlines)
+view(planes)
+view(weather)
+view(airports)
+view(flights)
+
+
+# Find the farthest airport
+airports %>%
+  semi_join(flights, by = c("faa" = "dest")) %>%
+  ggplot(aes(lon, lat)) +
+  borders("state") +
+  geom_point() +
+  coord_quickmap()
+
+# Join flights and airports to find the maximum distance
+flights_joined <- left_join(flights, airports, by = c("dest" = "faa"))
+
+# Filter to find the farthest airport
+farthest_airport <- flights_joined %>%
+  filter(distance == max(distance)) %>%
+  select(name) %>%
+  distinct()
+
+# Display the farthest airport
+print(farthest_airport)
